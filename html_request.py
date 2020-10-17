e19f42dd49eb663bd3f65ef9b9dcd2ba4c10d4c4
Awerito <diego.i.munoz.viveros@gmail.com>
2020-10-17 17:10:47 -0300

    Fix: Correct dictionary output.

--- html_request.py
+++ html_request.py
@@ -1,33 +1,35 @@
 from requests_html import HTMLSession
 
 
+INIT_POS = 103
+
 # Youtube search
 #search = input("Youtube Search: ")
 #search = str(search).replace(" ", "+").lower()
 #search = "https://www.youtube.com/results?search_query=%s" % search
-search = "https://www.youtube.com/results?search_query=wii+theme+10+hrs"
+search = "https://www.youtube.com/results?search_query=wii+theme"
 print(search)
 
 # Youtube request
 session = HTMLSession()
 r = session.get(search)
 r.html.render()
-print("Done!")
-
-lists_results = []
 
 # Video selection
 selection = "a#video-title"
 videos = r.html.find(selection)
+
+# Dictionary creation
+titles = {}
 for video in videos:
-    print(video)
+    string = str(video)
 
-    aux_position_1 = video[104:].find("'")
-    title = video[104:aux_position_1]
-    aux_postition_2 = video[(aux_position_1+7):].find("'")
-    link = video[(aux_position_1+7):aux_position_2] 
+    first_pos = string[INIT_POS:].find("'") + INIT_POS
+    second_pos = string[first_pos + 8:].find("'") + first_pos + 8
 
-    #dictionary results
-    dictionary_links{"title":title, "link":link}
-    
-    lists_results.append(dictionary_links)
+    title = string[INIT_POS:first_pos]
+    link = string[first_pos + 8:second_pos]
+
+    titles[title] = link
+
+print(titles)
