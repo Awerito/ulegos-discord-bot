7c86067ca6adfb4cb13116fbdafadc0bf432db38
Awerito <diego.i.munoz.viveros@gmail.com>
2020-10-17 15:13:19 -0300

    Add: output file.

--- html_request.py
+++ html_request.py
@@ -2,9 +2,10 @@ from requests_html import HTMLSession
 
 
 # Youtube search
-search = input("Youtube Search: ")
-search = str(search).replace(" ", "+").lower()
-search = "https://www.youtube.com/results?search_query=%s" % search
+#search = input("Youtube Search: ")
+#search = str(search).replace(" ", "+").lower()
+#search = "https://www.youtube.com/results?search_query=%s" % search
+search = "https://www.youtube.com/results?search_query=wii+theme+10+hrs"
 print(search)
 
 # Youtube request
