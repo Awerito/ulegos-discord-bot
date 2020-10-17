258aa821f1ffea9266c7265ba8b1c408bcd74396
Awerito <diego.i.munoz.viveros@gmail.com>
2020-10-17 14:31:38 -0300

    Add: html_request script.

---
+++ html_request.py
@@ -0,0 +1,20 @@
+from requests_html import HTMLSession
+
+
+# Youtube search
+search = input("Youtube Search: ")
+search = str(search).replace(" ", "+").lower()
+search = "https://www.youtube.com/results?search_query=%s" % search
+print(search)
+
+# Youtube request
+session = HTMLSession()
+r = session.get(search)
+r.html.render()
+print("Done!")
+
+# Video selection
+selection = "a#video-title"
+videos = r.html.find(selection)
+for video in videos:
+    print(video)
